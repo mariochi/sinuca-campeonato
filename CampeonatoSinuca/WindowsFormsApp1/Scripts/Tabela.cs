cd44a26927c28ec931ffb29759f08f06b91da4b2
José Cláudio Moretti Junior <jc.moretti.jr1991@gmail.com>
2021-07-18 01:01:15 -0300

    Commit inicial*
    Inicialização do projeto
    Criação das classes Tabela, TimeDeSinuca e TimeTabela
    Verificação de Times iguais baseados no nome
    Busca de times por Hash

---
+++ CampeonatoSinuca/WindowsFormsApp1/Scripts/Tabela.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Scripts
+{
+
+    class Tabela
+    {
+        private class PlacarTime : IEquatable<PlacarTime>
+        {
+            TimeDeSinuca timeRepresentado;
+            int placar;
+
+
+            public PlacarTime(TimeDeSinuca time)
+            {
+
+            }
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as PlacarTime);
+            }
+
+            public bool Equals(PlacarTime other)
+            {
+                return other != null &&
+                       EqualityComparer<TimeDeSinuca>.Default.Equals(timeRepresentado, other.timeRepresentado);
+            }
+        }
+        private const int MAX_TIMES_TABELA = 10;
+        private List<PlacarTime> timesNestaTabela;
+        public int pontosParaVitoria;
+        public string descricaoPremio;
+
+
+        public Tabela(int pontosVitoria, string descricao)
+        {
+            timesNestaTabela = new List<PlacarTime>();
+            pontosParaVitoria = pontosVitoria;
+            descricaoPremio = descricao;
+        }
+        public void AdicionaTime(int hash)
+        {
+            if (timesNestaTabela.Count >= MAX_TIMES_TABELA)
+            {
+                Console.WriteLine("Tabela cheia");
+                return;
+            }
+            TimeDeSinuca time = TimeDeSinuca.timesDisponiveis.Where(t => t.GetHashCode() == hash).FirstOrDefault();
+            if(time == null)
+            {
+                Console.WriteLine("Nenhum time com esse código");
+                return;
+            }
+            PlacarTime pt = new PlacarTime(time);
+            if(timesNestaTabela.Contains(pt))
+            {
+                Console.WriteLine("Esse time já está na tabela, retornando");
+                return;
+            }
+            timesNestaTabela.Add(pt);
+            
+            
+    
+        }
+        public void AdicionaTime(string Nome, string jog1, string jog2)
+        {
+            if(timesNestaTabela.Count >= MAX_TIMES_TABELA)
+            {
+                Console.WriteLine("Tabela cheia");
+                return;
+            }
+            TimeDeSinuca time = new TimeDeSinuca(Nome, jog1, jog2);
+            if(TimeDeSinuca.timesDisponiveis.Contains(time))
+            {
+                Console.WriteLine("Nome de time indisponível");
+                return;
+            }
+
+        }
+    }
+}
---
+++ CampeonatoSinuca/WindowsFormsApp1/Scripts/TimeDeSinuca.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Scripts
+{
+    class TimeDeSinuca : IEquatable<TimeDeSinuca>
+    {
+        public static List<TimeDeSinuca> timesDisponiveis;
+
+        public string NomeDoTime { get; private set; }
+        public string NomeDoJogador1 { get; private set; }
+        public string NomeDoJogador2 { get; private set; }
+
+
+        public TimeDeSinuca(string nome, string jog1, string jog2)
+        {
+            NomeDoTime = nome;
+            NomeDoJogador1 = jog1;
+            NomeDoJogador2 = jog2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TimeDeSinuca);
+        }
+
+        public bool Equals(TimeDeSinuca other)
+        {
+            return other != null &&
+                   NomeDoTime == other.NomeDoTime;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1994906337;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NomeDoTime);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NomeDoJogador1);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(NomeDoJogador2);
+            return hashCode;
+        }
+    }
+}
